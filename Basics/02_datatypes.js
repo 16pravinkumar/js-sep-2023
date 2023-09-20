a2d3189dbc9984009a0b875ae422826f95883991
_pravinsharma_16 <94048576+16pravinkumar@users.noreply.github.com>
2023-09-20 08:09:29 +0000

    datatypesSummary

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -11,8 +11,8 @@ console.log("Hitesh")
 
 let name = "hitesh"
 let age = 18
-let isLoggedIn = false
-let state;
+// let isLoggedIn = false
+// let state;
 
 // number => 2 to power 53
 // bigint
@@ -32,3 +32,57 @@ console.log(typeof null); // object
 // two imp links for study deeply
 // 1.   https://tc39.es/ecma262/
 // 2.   https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+
+
+// some other datatypes in js***********************************************
+
+
+// JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+
+console.log("***************************************************************************")
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+console.log("***************************************************************************")
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
