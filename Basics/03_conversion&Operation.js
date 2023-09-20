de5027983e65b4f50685e15f2342aeca1485716c
_pravinsharma_16 <94048576+16pravinkumar@users.noreply.github.com>
2023-09-20 07:45:04 +0000

    comparison

--- Basics/03_conversion&Operation.js
+++ Basics/03_conversion&Operation.js
@@ -44,17 +44,19 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
+
+// the below code are not valid code it is only used for tricky problem
 // console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
-// console.log("1" + 2 + 2);
+// console.log("1" + 2 + 2 +2);
 // console.log(1 + 2 + "2");
 
 // console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
-// console.log(+"");
+console.log(+"  ");
 
 let num1, num2, num3
 
@@ -64,5 +66,12 @@ let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
 
+
+// let x = 3;
+// // const y = ++x;
+// console.log(x+ "" +y)
+
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
