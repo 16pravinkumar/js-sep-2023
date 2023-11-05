1393d5b418b89e85eb11cb8707da07b2bc74c849
_pravinsharma_16 <94048576+16pravinkumar@users.noreply.github.com>
2023-11-05 12:24:48 +0000

    execute

--- 08_Api/execution_context_lexical_environment.js
+++ 08_Api/execution_context_lexical_environment.js
@@ -1,5 +1,41 @@
-// execution context matlab jab bhi hum function chalaayege function apna ek khudka ek imaginary container bana lega jismein uski teen cheeje hogi :
-// 1) variables
-// 2) function inside that parent function
-// 3) lexical environment of that function 
-// ye jo container hai imaginary ise hi hum execution context kahte hai
+
+// js 
+// --word vs keyword
+// -- var const and let
+// -- the difference 
+// -- window Object
+// -- browser context api
+// -- stack 
+// -- heap
+// -- execution context 
+// -- lexical environment
+// -- hoisting
+// -- types in js
+// -- how to copy referene value
+// -- conditionals
+// -- if else else if
+// -- truthy vs falsy
+// -- switch
+// -- first class fun eg  er
+
+// // execution context matlab jab bhi hum function chalaayege function apna ek khudka ek imaginary container bana lega jismein uski teen cheeje hogi :
+// // 1) variables
+// // 2) function inside that parent function
+// // 3) lexical environment of that function 
+// // ye jo container hai imaginary ise hi hum execution context kahte hai
+
+
+// // lexiacal environment hota hai ek chart jisme ye likha hota hai ke apka particular func ki cheejo ko access kar skta hai and kinko nahi, matlab ki it holds its scope and scope chain
+
+
+// ********************************************************************
+// -- how to copy referene value -> with the help of spread operator
+// var a = [1,2,3,4,5,6]
+// var b = [...a]
+// b.pop()
+// console.log(a,b)
+
+
+// truthy falsey
+// falsy value -> 0 false undefined null NaN document.all
+
